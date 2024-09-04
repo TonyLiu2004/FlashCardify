232c4beda7429033dacaa12afe39eae6a6aee51a
muzman123 <muzammilmuhammad12@gmail.com>
2024-09-03 20:09:21 -0600

    added contact page frontend

---
+++ app/contact/page.tsx
@@ -0,0 +1,66 @@
+"use client"
+
+import React, { useState } from 'react';
+//import { SMTPClient } from 'emailjs';
+
+const ContactPage: React.FC = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        // Send email using EmailJS
+    };
+
+    return (
+        <div className="max-w-lg mx-auto my-20 p-8 bg-black shadow-md rounded-lg">
+        <h1 className="text-2xl font-semibold text-center mb-6">Help us help you better!</h1>
+        <p className="text-center text-white">Have ideas to make FlashCardify even better? Share your thoughts with us below, and we'll get back to you shortly!</p>
+        <form onSubmit={handleSubmit} className="space-y-4 my-10">
+            <div>
+                <label htmlFor="name" className="block text-sm font-medium text-white">Name:</label>
+                <input
+                    type="text"
+                    id="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                    className="mt-1 block w-full px-3 py-2 border text-black border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+            </div>
+            <div>
+                <label htmlFor="email" className="block text-sm font-medium text-white">Email:</label>
+                <input
+                    type="email"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    className="mt-1 block w-full px-3 py-2 border text-black border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+            </div>
+            <div>
+                <label htmlFor="message" className="block text-sm font-medium text-white">Message:</label>
+                <textarea
+                    id="message"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    required
+                    className="mt-1 block w-full px-3 py-2 border text-black border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+            </div>
+            <button
+                type="submit"
+                className="w-full bg-indigo-600 text-white py-2 px-4 rounded-md shadow hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+                Send
+            </button>
+        </form>
+    </div>
+    
+    );
+};
+
+export default ContactPage;
