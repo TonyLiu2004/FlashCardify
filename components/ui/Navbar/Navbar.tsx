0b9f701a91701fe075d20da8f2933ee53827d595
Brian <brianshao20@gmail.com>
2024-09-04 12:26:25 -0400

    adjusted Navbar height to fix nav elements hitting bottom edge.

--- components/ui/Navbar/Navbar.tsx
+++ components/ui/Navbar/Navbar.tsx
@@ -11,7 +11,7 @@ export default async function Navbar() {
 
   return (
     <nav className={s.root} style={{
-      height:"8vh",
+      height:"12vh",
       position: "fixed", 
       top: 0, 
       left: 0, 
